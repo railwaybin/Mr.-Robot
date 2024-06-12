8b94d4591fe462fe3a76c79b93424d619f74bcb8
railwaybin <163636809+railwaybin@users.noreply.github.com>
2024-06-12 13:49:54 +0530

    Update godfather.js

--- godfather.js
+++ godfather.js
@@ -1,7 +1,7 @@
 /*F E N I X - V 15
              MADE BY F E N I X ID SERVER
        
-✅ WhatsApp: wa.me/94773010580
+✅ WhatsApp: wa.me/94704461999
 👥 Github: https://github.com/Fenix-server
 👥 Github: https://github.com/Botdevillozan
 🤖 Youtube https://youtube.com/@fenix_programmer
